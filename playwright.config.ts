cfb0c8e8c5be1f2a5ccce29a0d75c398044b8b17
Bobby Brylle Einstein Quijalvo <bqui9958@phil.convergys.com>
2026-07-27 16:46:01 +0800

    Add Allure reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['html'],
+  ['allure-playwright', {
+    detail: true,
+    outputFolder: 'allure-results',
+    suiteTitle: true,
+  }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     trace: 'on',
